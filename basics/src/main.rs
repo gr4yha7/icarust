86876bbcfe1a6a2e5455798af46daa51448fe822
gr4yha7 <jamaal.ekpenyong22@gmail.com>
2021-09-06 23:55:48 +0100

    control flow(1)

--- basics/src/main.rs
+++ basics/src/main.rs
@@ -1,9 +1,13 @@
-use std::io;
 
 fn main() {
-  println!("Enter your age:");
-  let mut age = String::new();
-  io::stdin().read_line(&mut age).expect("failed to read line");
-  let age: usize = age.trim().parse().expect("age is not a number");
-  println!("Age = 0x{:x}", age)
+  let mut counter: u32 = 0;
+  let result = loop {
+    counter += 1;
+
+    if counter >= 10 {
+      break counter*2;
+    }
+  };
+
+  println!("Result = {}", result)
 }
